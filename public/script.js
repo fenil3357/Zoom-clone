4cd56a39cd3503f2596151f1f94820cc737129a6
Fenil <fenil3357@gmail.com>
2022-08-22 12:24:32 +0530

    Commments and controls working

--- public/script.js
+++ public/script.js
@@ -16,13 +16,14 @@ var peer = new Peer(undefined, {
 
 let myVideoStream;
 
-navigator.mediaDevices.getUserMedia({
+navigator.mediaDevices.getUserMedia({     // Permission for mediaDevices
     video: true,
     audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
 
+    // If new user joins room
     peer.on('call', call => {
         call.answer(stream)
         const video = document.createElement('video')
@@ -30,20 +31,48 @@ navigator.mediaDevices.getUserMedia({
             addVideoStream(video, userVideoStream)
         })
     })
+    
 
+    // Socket event handling for 'user-connected'
     socket.on('user-connected', userId => {
             // user is joining`
             setTimeout(() => {
             // user joined
             connectToNewUser(userId, stream)
-        }, 1000)
+            }, 1000)
+        })
+
+
+    // User's input message
+    let textMsg = $('input')
+    
+
+    // Socket event 'message' when  user type message and hit enter
+    $('html').keydown((e) => {
+        if(e.which == 13 && textMsg.val().length !== 0) {
+            // console.log(textMsg.val());
+            socket.emit('message', textMsg.val());
+            textMsg.val('')
+        }
     })
+
+    // Socket evenet handling for incoming message 
+    socket.on('createMessage', message => {
+        // console.log('this message is coming from server', message)
+
+        $('.messages').append(`<li class="message"><b>user</b><br/>${message}</li>`)
+        scrollToBottom();
+    })
+
 })
 
+// When user joins room
 peer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
 })
 
+
+// Connecting to new user
 function connectToNewUser(userId, stream){
     const call = peer.call(userId, stream);
     const video = document.createElement('video')
@@ -52,6 +81,8 @@ function connectToNewUser(userId, stream){
     })
 }
 
+
+// This function will append a new video stream into videoGrid
 function addVideoStream(video, stream) {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
@@ -60,13 +91,79 @@ function addVideoStream(video, stream) {
     videoGrid.append(video);
 }
 
-let textMsg = $('input')
-// console.log(msg)
 
-$('html').keydown((e) => {
-    if(e.which == 13 && textMsg.val().length !== 0) {
-        console.log(textMsg.val());
-        socket.emit('message', textMsg.val());
-        textMsg.val('')
+// If chat window overflow 
+const scrollToBottom = () => {
+    let d = $('.main_chat_window')
+    d.scrollTop(d.prop("scrollHeight"))
+}
+
+
+// mute or unmute  audio
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+
+    if(enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
     }
-})
+    else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+
+// This function will make changes to the frontend when user turn on his audio
+const setMuteButton = () => {
+    const html = `
+        <i class="fas fa-microphone"></i>
+        <span>Mute</span>
+    `
+    document.querySelector('.main_mute_button').innerHTML = html;
+}
+
+
+// This function will make changes to the frontend when user turn off his audio
+const setUnmuteButton = () => {
+    const html = `
+        <i class="unmute fas fa-microphone-slash"></i>
+        <span>Unmute</span>
+    `
+    document.querySelector('.main_mute_button').innerHTML = html;
+}
+
+
+// mute or unmute video
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+
+    if(enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    }
+    else {
+        setStopVideo();
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+
+// This function will make changes to the frontend when user turn on his video
+const setStopVideo = () => {
+    const html = `
+        <i class="fas fa-video"></i>
+        <span>Stop Video</span>
+    `
+    document.querySelector('.main_video_button').innerHTML = html;
+}
+
+
+// This function will make changes to the frontend when user turn off his video
+const setPlayVideo = () => {
+    const html = `
+        <i class="unmute stop fas fa-video-slash"></i>
+        <span>Play Video</span>
+    `
+    document.querySelector('.main_video_button').innerHTML = html;
+}
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const { Socket } = require('dgram');
+// Setting up express app
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+
+// uuid v4 (to create room id)
 const { v4: uuidv4 } = require('uuid');
+
+// Setting up view engine as ejs 
 app.set('view engine', 'ejs');
+
+// Static files
 app.use(express.static('public'));
+
+// Socketio
+const { Socket } = require('dgram');
 const io = require('socket.io')(server)
 
 // Importing Peer
@@ -13,19 +22,33 @@ const peerServer = ExpressPeerServer(server, {
     debug: true
 });
 
+
+// A server side element to broker connections between PeerJS clients.
 app.use('/peerjs', peerServer);
+
+
+// It will create a new room for user with new roomId using uuidv4
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidv4()}`);
+    res.redirect(`/${uuidv4()}`);      
 });
 
+
+// If user join with custom roomId 
 app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room });
 });
 
+
+// Socketio events
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
+
         socket.to(roomId).emit('user-connected', userId);
+
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message)
+        })
     })
 })
 
